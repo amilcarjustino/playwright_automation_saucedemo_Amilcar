d17a7b48032ec51f639ec9c476263ba31db72e8a
Amilcar Justino <amilcarjustino@gmail.com>
2026-05-18 00:40:27 +0100

    extract hard coded strings

--- tests/PageObjects/ProductsPage.ts
+++ tests/PageObjects/ProductsPage.ts
@@ -1,20 +1,27 @@
-import { expect, Page } from "@playwright/test";
+import { expect, Locator, Page } from "@playwright/test";
+
+const INVENTORY_URL = "/inventory.html";
+const PAGE_HEADER_TITLE = "Swag Labs";
+const PRODUCTS_TITLE = "Products";
 
 export class ProductsPage {
   readonly page: Page;
+  readonly locatorPageTitle: Locator;
+  readonly locatorProductsTitle: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    this.locatorPageTitle = this.page.getByText(PAGE_HEADER_TITLE);
+    this.locatorProductsTitle = this.page.getByText(PRODUCTS_TITLE);
   }
 
   async goto() {
-    await this.page.goto("/inventory.html");
+    await this.page.goto(INVENTORY_URL);
   }
 
   async assertPageVisible() {
-    await expect(this.page).toHaveURL("/inventory.html");
-    await expect(this.page.getByText("Swag Labs")).toBeVisible();
-    await expect(this.page.getByText("Products")).toBeVisible();
+    await expect(this.page).toHaveURL(INVENTORY_URL);
+    await expect(this.locatorPageTitle).toBeVisible();
+    await expect(this.locatorProductsTitle).toBeVisible();
   }
-
 }
