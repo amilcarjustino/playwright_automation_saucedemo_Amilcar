11972358cbdb28baac04a64827b31d152922ef5c
Amilcar Justino <amilcarjustino@gmail.com>
2026-05-18 23:51:40 +0100

    update products page object

--- tests/PageObjects/ProductsPage.ts
+++ tests/PageObjects/ProductsPage.ts
@@ -3,6 +3,11 @@ import { expect, Locator, Page } from "@playwright/test";
 const INVENTORY_URL = "/inventory.html";
 const PAGE_HEADER_TITLE = "Swag Labs";
 const PRODUCTS_TITLE = "Products";
+const ADD_TO_CART_BUTTON_NAME = "Add to cart";
+const INVENTORY_ITEM_DESCRIPTION_SELECTOR =
+  '[data-test="inventory-item-description"]';
+const SHOPPING_CART_LINK_SELECTOR = '[data-test="shopping-cart-link"]';
+const FIRST_PRODUCT_DESCRIPTION = "Sauce Labs Backpack";
 
 export class ProductsPage {
   readonly page: Page;
@@ -17,11 +22,25 @@ export class ProductsPage {
 
   async goto() {
     await this.page.goto(INVENTORY_URL);
+    await this.assertPageVisible();
   }
 
   async assertPageVisible() {
     await expect(this.page).toHaveURL(INVENTORY_URL);
     await expect(this.locatorPageTitle).toBeVisible();
     await expect(this.locatorProductsTitle).toBeVisible();
+  }
+
+  async addFirstProductToCart() {
+    const product = this.page
+      .locator(INVENTORY_ITEM_DESCRIPTION_SELECTOR)
+      .filter({ hasText: FIRST_PRODUCT_DESCRIPTION });
+    await product
+      .getByRole("button", { name: ADD_TO_CART_BUTTON_NAME })
+      .click();
+  }
+
+  async gotoCart() {
+    await this.page.locator(SHOPPING_CART_LINK_SELECTOR).click();
   }
 }
