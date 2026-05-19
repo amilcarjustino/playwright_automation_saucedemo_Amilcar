caf74b049d20ae2c7e635ee1b516f48b01638abf
Amilcar Justino <amilcarjustino@gmail.com>
2026-05-19 22:54:16 +0100

    add new test

--- tests/e2e/authentication/authenticate.setup.ts
+++ tests/e2e/authentication/authenticate.setup.ts
@@ -1,6 +1,6 @@
 import { test as setup } from "@playwright/test";
-import { LoginPage } from "./PageObjects/LoginPage";
-import { ProductsPage } from "./PageObjects/ProductsPage";
+import { LoginPage } from "../../PageObjects/LoginPage";
+import { ProductsPage } from "../../PageObjects/ProductsPage";
 
 setup("authenticate standard user", async ({ page }) => {
   const loginPage = new LoginPage(page);
---
+++ tests/e2e/products/purchaseProduct.spec.ts
@@ -0,0 +1,73 @@
+import { test } from "@playwright/test";
+import { ProductsPage } from "../../PageObjects/ProductsPage";
+import { CartPage } from "../../PageObjects/CartPage";
+import { CheckoutInformationPage } from "../../PageObjects/CheckoutInformationPage";
+import { CheckoutConfirmationPage } from "../../PageObjects/CheckoutConfirmationPage";
+import { CheckoutOverviewPage } from "../../PageObjects/CheckoutOverview";
+
+// test.beforeEach(async ({ page }) => {
+//   const loginPage = new LoginPage(page);
+//   await loginPage.gotoLoginPage();
+// });
+
+test.describe("Product purchase", () => {
+  test("Purchase a product", async ({ page }) => {
+    const product = {
+      name: "Sauce Labs Backpack",
+      description:
+        "carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.",
+      quantity: "1",
+      price: 29.99,
+      paymentInformation: "SauceCard #31337",
+      shippingInformation: "Free Pony Express Delivery!",
+      itemTotal: 29.99,
+      tax: 2.40,
+    };
+    const userInformation = {
+      firstName: "Amil",
+      lastName: "Car",
+      postalCode: "2415",
+    };
+
+    const products = new ProductsPage(page);
+    await products.goto();
+    await products.addProductToCart(product.name);
+    await products.clickGotoCart();
+
+    const cart = new CartPage(page);
+    await cart.assertPageVisible();
+    await cart.assertProductInCart(
+      product.name,
+      product.description,
+      product.quantity,
+      product.price,
+    );
+    await cart.clickCheckout();
+
+    const checkoutInformation = new CheckoutInformationPage(page);
+    await checkoutInformation.assertPageVisible();
+    await checkoutInformation.fillCustomerInformation(
+      userInformation.firstName,
+      userInformation.lastName,
+      userInformation.postalCode,
+    );
+    await checkoutInformation.clickContinueButton();
+
+    const checkoutOverview = new CheckoutOverviewPage(page);
+    await checkoutOverview.assertPageVisible();
+    await checkoutOverview.assertOverviewDetails(
+      product.name,
+      product.description,
+      product.quantity,
+      product.price,
+      product.paymentInformation,
+      product.shippingInformation,
+      product.itemTotal,
+      product.tax,
+    );
+    await checkoutOverview.clickFinishButton();
+
+    const confirmation = new CheckoutConfirmationPage(page);
+    await confirmation.assertPageVisible();
+  });
+});
